6ef4ff94c99e54136e9fca262c93f30f1451aa48
Sümeyra Adıyaman <250541012@firat.edu.tr>
2025-10-22 23:12:19 +0300

    Add DikdortgenHesap class for rectangle calculations

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,13 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+        double alan = boy * en;
+        double cevre = 2 * (en + boy);
+        System.out.println("en = " + en);
+        System.out.println("boy = " + boy);
+        System.out.println("alan = " + alan);
+        System.out.println("cevre = " + cevre);
+
+    }
+}
