28a6b361c24f33fbb607aa965ced9fe3c1f4a8c7
Sümeyra Adıyaman <250541012@firat.edu.tr>
2025-10-22 23:43:45 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,16 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Sümeyra";
+        String soyad = "Adıyaman";
+        String ogrNo = "250541012";
+        String bolum = "Yazılım Mühendisliği";
+        System.out.println("===================");
+        System.out.println(" ÖĞRENCİ BİLGİLERİ ");
+        System.out.println("===================");
+        System.out.println("Ad       = " + ad);
+        System.out.println("Soyad    = " + soyad);
+        System.out.println("OgrNo    = " + ogrNo);
+        System.out.println("Bolum    = " + bolum);
+        System.out.println("===================");
+    }
+}
