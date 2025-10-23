4cac90a789aec8102bd7df1897b6732d8dbb3bb1
Sümeyra Adıyaman <250541012@firat.edu.tr>
2025-10-23 11:44:13 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,11 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+    int mil = 1609;
+    System.out.println("MİL" +    "          " + "KİLOMETRE");
+    System.out.println("1 mil" +  "          " + (mil*1) +  "km");
+    System.out.println("5 mil" +  "          " + (mil*5) +  "km");
+    System.out.println("10 mil" + "         " + (mil*10) + "km");
+    System.out.println("20 mil" + "         " + (mil*20) + "km");
+    System.out.println("50 mil" + "         " + (mil*50) + "km");
+    }
+}
