e9e69147020fc28de21aaaaacd061c4be6b40d92
Sümeyra Adıyaman <250541012@firat.edu.tr>
2025-10-23 11:52:30 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,15 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        System.out.println(" 2'NİN ÇARPIM TABLOSU");
+        System.out.println("      1*2 = " + 1*2);
+        System.out.println("      2*2 = " + 2*2);
+        System.out.println("      3*2 = " + 3*2);
+        System.out.println("      4*2 = " + 4*2);
+        System.out.println("      5*2 = " + 5*2);
+        System.out.println("      6*2 = " + 6*2);
+        System.out.println("      7*2 = " + 7*2);
+        System.out.println("      8*2 = " + 8*2);
+        System.out.println("      9*2 = " + 9*2);
+        System.out.println("      10*2 = " + 10*2);
+    }
+}
